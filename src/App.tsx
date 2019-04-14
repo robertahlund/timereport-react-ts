3091fc164be2f6eb9babd3c11263e42355148359
Robert Åhlund <robert.ahlund@gmail.com>
2019-04-14 21:56:21 +0200

    yes

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,12 @@ import "./App.css";
 import { Menu } from "./components/Menu";
 import { MenuItem } from "./components/MenuItem";
 
-class App extends Component {
-  componentDidMount(): void {}
+interface AppState {
+
+}
+
+class App extends Component<{}, AppState> {
+  componentDidMount = (): void => {};
 
   x = (event: React.MouseEvent): void => {
     console.log(event.target);
@@ -15,25 +19,10 @@ class App extends Component {
     return (
       <div className="App">
         <Menu title="Testeroni">
-          <MenuItem text="Cocks" />
+          <MenuItem text="qwe" />
           <MenuItem text="Abc" />
           <MenuItem text="Qwerty" />
         </Menu>
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-            onClick={this.x}
-          >
-            Learn React
-          </a>
-        </header>
       </div>
     );
   }
