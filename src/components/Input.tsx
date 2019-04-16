eb796c21a950c3a75b9974b45dac1a6e0c25c430
Robert Åhlund <robert.ahlund@brightskills.se>
2019-04-16 17:44:54 +0200

    css

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,24 +1,6 @@
 import React, {Fragment, FunctionComponent} from 'react';
 import styled from "styled-components";
 
-const Label = styled.label`
-    display: block;
-    font-size: 14px;
-    font-weight: 500;
-    padding: 0 0 3px 0;
-    &:not(:first-child) {
-        padding-top: 15px;
-    }
-`;
-
-const InputField = styled.input`
-    background-color: #FBFBFB;
-    border-radius: 3px;
-    border: 1px solid #F1F1F1;
-    padding: 10px;
-    width: 300px;
-`;
-
 interface InputProps {
     value: string;
     labelValue: string;
@@ -29,12 +11,33 @@ interface InputProps {
 }
 
 const Input: FunctionComponent<InputProps> = props => {
+    const Label = styled.label`
+        display: block;
+        font-size: 14px;
+        font-weight: 500;
+    `;
+
+    const FormContainer = styled.div`
+        padding: 0 0 3px 0;
+        &:not(:first-child) {
+            padding-top: 15px;
+        }
+    `;
+
+    const InputField = styled.input`
+        background-color: #FBFBFB;
+        border-radius: 3px;
+        border: 1px solid #F1F1F1;
+        padding: 10px;
+        width: ${props.width};
+    `;
+
     return (
-        <Fragment>
+        <FormContainer>
             <Label htmlFor={props.name}>{props.labelValue}</Label>
             <InputField id={props.name} name={props.name} value={props.value} type={props.type}
                         onChange={(event: any) => props.onFormChange(event.target.value)} autoComplete="off"/>
-        </Fragment>
+        </FormContainer>
     );
 };
 
--- src/components/authentication/RegisterForm.tsx
+++ src/components/authentication/RegisterForm.tsx
@@ -1,5 +1,6 @@
 import React, {FunctionComponent} from 'react';
 import Input from "../Input";
+import styled from "styled-components";
 
 interface RegisterFormProps {
     email: string;
@@ -13,13 +14,23 @@ interface RegisterFormProps {
 }
 
 const RegisterForm: FunctionComponent<RegisterFormProps> = props => {
+    const Row = styled.div`
+        display: flex;
+    `;
     return (
         <form autoComplete="off">
-            <Input value={props.firstName} labelValue="First name" type="text" name="first-name" onFormChange={props.onFirstNameChange} width="300px"/>
-            <Input value={props.lastName} labelValue="Last name" type="text" name="last-name" onFormChange={props.onLastNameChange} width="300px"/>
-            <Input name="email" value={props.email} labelValue="Email" type="text" onFormChange={props.onEmailChange} width="300px"/>
-            <Input name="password" value={props.password} labelValue="Password" type="password"
-                   onFormChange={props.onPasswordChange} width="300px"/>
+            <Row>
+                <Input value={props.firstName} labelValue="First name" type="text" name="first-name"
+                       onFormChange={props.onFirstNameChange} width="48%"/>
+                <Input value={props.lastName} labelValue="Last name" type="text" name="last-name"
+                       onFormChange={props.onLastNameChange} width="48%"/>
+            </Row>
+            <Row>
+                <Input name="email" value={props.email} labelValue="Email" type="text"
+                       onFormChange={props.onEmailChange} width="48%"/>
+                <Input name="password" value={props.password} labelValue="Password" type="password"
+                       onFormChange={props.onPasswordChange} width="48%"/>
+            </Row>
         </form>
     )
 };
