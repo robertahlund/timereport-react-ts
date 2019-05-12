dc74711e434a4803bab4a000aba428b57c3b6bed
Robert Åhlund <robert.ahlund@gmail.com>
2019-05-12 22:49:15 +0200

    added regexp type

--- src/utilities/validate/validateRegisterForm.ts
+++ src/utilities/validate/validateRegisterForm.ts
@@ -11,7 +11,7 @@ export const validateRegisterForm = (form: RegisterFormValue): RegisterFormValue
 };
 
 export const validateEmail = (email: FormValue): FormValue => {
-  const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  const emailRegex: RegExp = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   const isValid: boolean = !_.isNil(email.value) && email.value.length > 0 && emailRegex.test(email.value.toLowerCase());
   let validationMessage: string = "";
   if (!isValid) {
